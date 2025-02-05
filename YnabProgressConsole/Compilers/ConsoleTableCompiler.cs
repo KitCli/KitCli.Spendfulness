74848908f6cfcc1c1dfd4c7b2d9bd3c5dcaf886c
Joshua Crowe <joshuaedwardcrowe@icloud.com>
2025-02-05 20:55:05 +0000

    Restructure Project to Use DI

--- YnabProgressConsole/Compilers/ConsoleTableCompiler.cs
+++ YnabProgressConsole/Compilers/ConsoleTableCompiler.cs
@@ -5,6 +5,7 @@ namespace YnabProgressConsole.Compilers;
 
 public static class ConsoleTableCompiler
 {
+    [Obsolete("Please use the Command Handler route")]
     public static ConsoleTable Compile(ViewModel viewModel)
     {
         var table = new ConsoleTable(viewModel.Columns.ToArray());
